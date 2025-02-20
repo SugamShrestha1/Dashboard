bab6c74e18fb276f7e4e1dc8d4c8491201eb135f
SugamShrestha1 <146341204+SugamShrestha1@users.noreply.github.com>
2025-02-20 09:52:34 +0545

    Update chart.tsx

--- src/components/chart/chart.tsx
+++ src/components/chart/chart.tsx
@@ -57,7 +57,7 @@ const data = [
 ];
 const Chart = () => {
     return (
-        <div className='w-full min-h-[400px]'>
+        <div className='w-[1000px] h-[700px] mt-9'>
             <ResponsiveContainer width="100%" height="100%">
                 <LineChart width={500} height={300} data={data}>
                     <CartesianGrid strokeDasharray="3 3" />
